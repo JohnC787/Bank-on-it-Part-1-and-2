fa9d0fea23e32d58511be1fab7abccda32ca7ef5
JohnC787 <johncolligan2005@yahoo.com>
2026-03-20 22:04:09 -0400

    Create CheckingAccount.java

---
+++ CheckingAccount.java
@@ -0,0 +1,11 @@
+switch (choice) {
+    case 1:
+        checkBalance();
+        break;
+    case 2:
+        makeDeposit();
+        break;
+    case 3:
+        makeWithdrawal();
+        break;
+}
