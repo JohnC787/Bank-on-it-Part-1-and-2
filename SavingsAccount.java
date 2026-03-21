677421f471065a80f85c48832b7d40523f004e5b
JohnC787 <johncolligan2005@yahoo.com>
2026-03-20 22:05:36 -0400

    Create SavingsAccount.java

---
+++ SavingsAccount.java
@@ -0,0 +1,30 @@
+public class SavingsAccount extends CheckingAccount {
+    private double interestRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(double balance, double interestRate) {
+        super(balance);
+        this.interestRate = interestRate;
+    }
+
+    public void setInterestRate(double rate) {
+        this.interestRate = rate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void calcInterest() {
+        double interest = getBalance() * interestRate;
+        setBalance(getBalance() + interest);
+    }
+
+    public static void main(String[] args) {
+        SavingsAccount acc = new SavingsAccount();
+        acc.start();
+    }
+}
