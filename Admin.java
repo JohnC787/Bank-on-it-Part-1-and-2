e2111c1dad0d38f2543647f68290270483441082
JohnC787 <johncolligan2005@yahoo.com>
2026-03-27 19:20:02 -0400

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -5,13 +5,12 @@ public class Admin extends User implements HasMenu {
     }
 
     @Override
-    public void menu() {
-        System.out.println("\nAdmin Menu");
-        System.out.println("0) Exit this menu");
-        System.out.println("1) Full customer report");
-        System.out.println("2) Add user");
-        System.out.println("3) Apply interest to savings accounts");
-        System.out.print("Action: ");
+    public String menu() {
+        return "\nAdmin Menu\n" +
+               "0) Exit this menu\n" +
+               "1) Full customer report\n" +
+               "2) Add user\n" +
+               "3) Apply interest to savings accounts\n";
     }
 
     @Override
